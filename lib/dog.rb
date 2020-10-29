08a03356ed7f1a33aeadc8b9e5a22f32e9c09c19
freddyflorial@gmail.com <freddyflorial@gmail.com>
2020-10-28 20:50:16 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+
+    @@all = []
+
+    attr_accessor :name #has a name
+    def initialize(name) #.new initializes an argument of a name
+        @name = name 
+        @save = save
+    end
+    def self.all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+    def self.clear_all
+        @@all.clear
+    end
+    def self.print_all
+        @@all.each do |dog|
+        puts dog.name
+        end
+    end
+    def save
+        @@all << self
+    end 
+end 
